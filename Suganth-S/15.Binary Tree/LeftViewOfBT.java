a9b0f1f6aa948ad6bf8de07c5922b3cc38e19847
Suganth S <33140295+Suganth-S@users.noreply.github.com>
2022-09-18 16:39:53 +0530

    Create LeftViewOfBT.java

---
+++ Suganth-S/15.Binary Tree/LeftViewOfBT.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public List<Integer> leftSideView(TreeNode root) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        resultList(arr, root, 0);
+        return arr;
+    }
+    
+    static void resultList (List<Integer> res, TreeNode root, int currLevel)
+    {
+        if(root==null)
+            return;
+        
+        if(currLevel == res.size())
+        {
+            res.add(root.val);
+        }
+        
+        resultList(res, root.left, currLevel+1);
+        resultList(res, root.right, currLevel+1);
+    }
+}
