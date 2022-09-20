80835e8dacad59e09bc8cd6a4c06c99f7909508e
Sushant Agawane <Sushant1.Agawane@ril.com>
2022-09-20 22:10:44 +0530

    Tree Traversals

---
+++ Sushant_Agawane/Step_13/13.1/PostorderTraverse.cpp
@@ -0,0 +1,13 @@
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+void postOrderTrav(node * curr, vector < int > & postOrder) {
+  if (curr == NULL)
+    return;
+
+  postOrderTrav(curr -> left, postOrder);
+  postOrderTrav(curr -> right, postOrder);
+  postOrder.push_back(curr -> data);
+}
---
+++ Sushant_Agawane/Step_13/13.1/PreorderTraverse.cpp
@@ -0,0 +1,13 @@
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+void preOrderTrav(node * curr, vector < int > & preOrder) {
+  if (curr == NULL)
+    return;
+
+  preOrder.push_back(curr -> data);
+  preOrderTrav(curr -> left, preOrder);
+  preOrderTrav(curr -> right, preOrder);
+}
