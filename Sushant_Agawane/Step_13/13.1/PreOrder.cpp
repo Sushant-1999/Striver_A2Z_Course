30293f484522953ef14755c2ec835dd29d8d5ce4
Sushant Agawane <Sushant1.Agawane@ril.com>
2022-09-20 22:53:09 +0530

    PreOrder Traversal of a Tree

---
+++ Sushant_Agawane/Step_13/13.1/PreOrder.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+        
+    void Preorder(TreeNode* root, vector<int> &ans) {
+        if(root == NULL) return;
+        ans.push_back(root->val);
+        Preorder(root->left, ans);
+        Preorder(root->right, ans);
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        //Preorder Traversal of a Tree using Recursion
+        vector<int> ans;
+        Preorder(root, ans);
+        return ans;
+    }
+};
