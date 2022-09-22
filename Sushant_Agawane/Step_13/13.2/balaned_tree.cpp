6ebc849177a207c570179b6851d029bf4d54ec65
Sushant Agawane <Sushant1.Agawane@ril.com>
2022-09-22 22:11:36 +0530

    Balanced Binary Tree Check

---
+++ Sushant_Agawane/Step_13/13.2/balaned_tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return heightDfs(root) != -1;
+    }
+    
+    int heightDfs(TreeNode* root) {
+        if(root == NULL) return 0;
+        
+        int lh = heightDfs(root->left);
+        if(lh == -1) return -1;
+        
+        int rh = heightDfs(root->right);
+        if(rh == -1) return -1;
+        
+        if(abs(lh-rh) > 1) return -1;
+        
+        
+        return 1 + max(lh,rh);
+    }
+};
