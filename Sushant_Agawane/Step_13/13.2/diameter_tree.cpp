6c11eb9757f6592b20588db47cc97942b4d438cc
Sushant Agawane <Sushant1.Agawane@ril.com>
2022-09-23 21:11:15 +0530

    Diameter of Binary Tree

---
+++ Sushant_Agawane/Step_13/13.2/diameter_tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        //Here we are finding left height and right height for each one of the node and updating the max variable for every level.
+        int diameter = 0;
+        findHeight(root, diameter);
+        return diameter;
+        
+    }
+    
+    int findHeight(TreeNode* root, int& diameter) {
+        if(root == NULL) return 0;
+        
+        int lh = findHeight(root->left, diameter);
+        int rh = findHeight(root->right, diameter);
+        
+        diameter = max(diameter, lh+rh);
+        
+        return 1 + max(lh, rh);
+    }
+};
