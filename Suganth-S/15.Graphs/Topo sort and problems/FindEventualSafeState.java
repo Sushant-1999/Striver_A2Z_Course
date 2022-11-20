f5951b389730be604f0e928fd3a259587653f6d4
Suganth S <33140295+Suganth-S@users.noreply.github.com>
2022-11-20 23:00:38 +0530

    Create FindEventualSafeState.java

---
+++ Suganth-S/15.Graphs/Topo sort and problems/FindEventualSafeState.java
@@ -0,0 +1,46 @@
+class Solution {
+    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) {
+        List<List<Integer>> adjRev = new ArrayList<>();
+        for(int i=0; i<V; i++)
+            {
+                adjRev.add(new ArrayList<>());
+            }
+        
+        int indegree[] = new int[V];
+        for(int i=0; i<V; i++)
+        {
+            for(int it: adj.get(i))
+            {
+                adjRev.get(it).add(i);
+                indegree[i]++;
+            }
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> safeNode = new ArrayList<>();
+        for(int i=0; i<V; i++)
+        {
+            if(indegree[i] == 0)
+            {
+                q.add(i);
+            }
+        }
+        
+        while(!q.isEmpty())
+        {
+            int node = q.peek();
+            q.remove();
+            safeNode.add(node);
+            for(int it: adjRev.get(node))
+            {
+                indegree[it]--;
+                if(indegree[it] == 0)
+                {
+                    q.add(it);
+                }
+            }
+        }
+        Collections.sort(safeNode);
+        return safeNode;
+    }
+}
